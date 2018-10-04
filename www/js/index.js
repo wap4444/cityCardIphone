60af6eeaa274f62cc528301822143d0faf899c21
wap4444 <wap4444@mail.ru>
2018-10-04 14:42:26 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -17,6 +17,10 @@ document.addEventListener("online", onOnline, false);
  
 function onOnline() {
 var ref = cordova.InAppBrowser.open('http://citycard.smart-pavlodar.kz/app/?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
+}
+
+
+$("#BtnEnt" ).click(function() {
   cordova.plugins.barcodeScanner.scan(
       function (result) {
           alert("We got a barcode\n" +
@@ -42,35 +46,7 @@ var ref = cordova.InAppBrowser.open('http://citycard.smart-pavlodar.kz/app/?push
       }
    );
 
-}
-if(localStorage.ipush){}
-else{}
+});
 
-$("#BtnEnt" ).click(function() {
-var ref = cordova.InAppBrowser.open('http://citycard.smart-pavlodar.kz/app/?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
-});
-        
-function didReceiveRemoteNotificationCallBack(jsonData) {}
-function didOpenRemoteNotificationCallBack(jsonData) {
-alert('PUSH'+jsonData.notification.payload.additionalData.ssylka);
-var ref = cordova.InAppBrowser.open(jsonData.notification.payload.additionalData.ssylka, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
-}     
-        //Настройка ПУШЕЙ ДЛЯ АЙФОНА
-        var iosSettings = {};
-        iosSettings["kOSSettingsKeyAutoPrompt"] = true;
-        iosSettings["kOSSettingsKeyInAppLaunchURL"] = true;
-        //ПОДКЛЮЧЕНИЕ ПУШЕЙ 
-           window.plugins.OneSignal
-          .startInit("6e9818ed-954b-48fe-96cd-3622d5252ffa")
-          .handleNotificationReceived(didReceiveRemoteNotificationCallBack)
-	  .handleNotificationOpened(didOpenRemoteNotificationCallBack)
-          .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.None)
-          .iOSSettings(iosSettings)
-          .endInit();
-window.plugins.OneSignal.getIds(function(ids) {
-ipush = ids.userId;
-localStorage.ipush=ipush;
-var ref = cordova.InAppBrowser.open('http://citycard.smart-pavlodar.kz/app/?push='+ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
-});
     }
 };
