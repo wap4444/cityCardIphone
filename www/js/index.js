e81b3dc6d234be61807918e2e9eae6892fa954f7
wap4444 <wap4444@mail.ru>
2018-10-05 00:00:41 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -18,6 +18,29 @@ document.addEventListener("online", onOnline, false);
 function onOnline() {
 var ref = cordova.InAppBrowser.open('http://citycard.smart-pavlodar.kz/app/?push='+localStorage.ipush, '_blank', 'location=no,toolbar=no,disallowoverscroll=yes');
 }
+        
+        
+function didReceiveRemoteNotificationCallBack(jsonData) {}
+function didOpenRemoteNotificationCallBack(jsonData) {}       
+        //Настройка ПУШЕЙ ДЛЯ АЙФОНА
+        var iosSettings = {};
+        iosSettings["kOSSettingsKeyAutoPrompt"] = true;
+        iosSettings["kOSSettingsKeyInAppLaunchURL"] = true;
+
+        //ПОДКЛЮЧЕНИЕ ПУШЕЙ 
+           window.plugins.OneSignal
+          .startInit("6e9818ed-954b-48fe-96cd-3622d5252ffa")
+          .handleNotificationReceived(didReceiveRemoteNotificationCallBack)
+          .handleNotificationOpened(didOpenRemoteNotificationCallBack)
+		  .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.None)
+          .iOSSettings(iosSettings)
+          .endInit();
+        
+window.plugins.OneSignal.getIds(function(ids) {
+ipush = ids.userId;
+localStorage.ipush=ipush;
+    alert(localStorage.ipush);
+});
 
 
 $("#BtnEnt" ).click(function() {
