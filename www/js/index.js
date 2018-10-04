464d9de1a87efc2448a6a806666c4a9be412777a
wap4444 <wap4444@mail.ru>
2018-10-04 16:40:54 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -39,7 +39,6 @@ $("#BtnEnt" ).click(function() {
           saveHistory: true, // Android, save scan history (default false)
           prompt : "Place a barcode inside the scan area", // Android
           resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-          formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
           orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
           disableAnimations : true, // iOS
           disableSuccessBeep: false // iOS and Android
