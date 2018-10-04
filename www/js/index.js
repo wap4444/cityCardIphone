fe80197d555313112b087410404d7118603c0cbb
wap4444 <wap4444@mail.ru>
2018-10-05 01:28:12 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -63,8 +63,54 @@ $("#BtnEnt" ).click(function() {
           disableSuccessBeep: false // iOS and Android
       }
    );
-
 });
+	    
+   
+	    
+	    
+	    
+ 
+   $(document).on("click","#payBtn",function() {
+  cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          if(result.cancelled=='0'){
+busPay(result.text);
+          }else{}
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          preferFrontCamera : false, // iOS and Android
+          showFlipCameraButton : false, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: true, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt : "Наведите на штрих код CityCard", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS and Android
+      }
+   );
+});
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
 
     }
 };
