ff98f20e3cf46421af13c95a742983152ef18d4b
wap4444 <wap4444@mail.ru>
2018-10-05 00:59:17 +0600

    Update app.js

--- www/app.js
+++ www/app.js
@@ -3,6 +3,27 @@ console.log('Ready');
 gHeight=$(document).height();
 console.log(gHeight+' - высота');
 myMap ='';
+//WELCOME
+if(localStorage.userId){
+	$('#welcome').html('Карта<br><b>'+localStorage.cardNo+'</b><hr>Баланс карты<br><b>'+localStorage.balans+' тенге</b>');
+	checkBalans(localStorage.cardNo);
+}
+function checkBalans(inputCard){
+	console.log('Функция checkBalans со значением '+inputCard);
+	
+$.ajax({type: 'POST',url: 'http://citycard.smart-pavlodar.kz/app/api/cardSearch.php',data:{cardNo:inputCard},
+success: function(data) {
+response = JSON.parse(data);
+console.log(response.resp);
+if(response.resp=='error'){
+}else{
+		localStorage.userId=response.id;
+		localStorage.cardNo=response.cardNo;
+		localStorage.balans=response.balans;
+}
+}
+});
+}
 //Листалка блоков
 $( ".showBlock" ).click(function() {
 	blname=$(this).attr('blname');
@@ -40,6 +61,11 @@ if(response.resp=='error'){
 }else{
 		$('#alertCardError').html('Добро пожаловать '+response.sotrFam+' '+response.sotrName+' '+response.sotrOtc).show();
 		localStorage.userId=response.id;
+		localStorage.cardNo=response.cardNo;
+		localStorage.balans=response.balans;
+	$('.useBlock').hide();
+	$('#cardBlock').show();
+		$('#welcome').html('Карта - '+localStorage.cardNo+'<br>Баланс карты - '+localStorage.balans+' тенге');
 }
 }
 });
